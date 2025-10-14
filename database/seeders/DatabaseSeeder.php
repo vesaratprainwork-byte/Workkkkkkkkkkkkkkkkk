3a95cdddfbaf13cbe6b01197bb8fc8bd849fce35
john doe <johndoe@ecample.com>
2025-10-14 20:06:02 +0700

    ครั้งที่3

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,8 @@
 
 namespace Database\Seeders;
 
+use App\Models\Movie;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -11,11 +13,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        
         $this->call([
-            UserSeeder::class,
-            GenreSeeder::class,
-            ProviderSeeder::class, 
-            MovieSeeder::class, 
+            UserSeeder::class,      
+            GenreSeeder::class,     
+            ProviderSeeder::class,  
+            MovieSeeder::class,     
         ]);
+
+        
+        $users = User::factory(10)->create();
+
+        
+        $movieIds = Movie::pluck('id');
+
+       
+        foreach ($users as $user) {
+           
+            $moviesToReview = $movieIds->random(rand(1, 3));
+            
+            foreach ($moviesToReview as $movieId) {
+              
+                \App\Models\Review::factory()->create([
+                    'user_id' => $user->id,
+                    'movie_id' => $movieId,
+                ]);
+            }
+        }
     }
 }
